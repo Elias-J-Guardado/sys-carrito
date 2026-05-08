64477166be95056767e906aaa574f5bf32ec9d92
Elias Guardado <eliasjguardado2001@gmail.com>
2026-05-08 14:11:13 -0600

    Fix InventarioLoading component and adding bootstrap styles

--- src/InventarioLoading/index.jsx
+++ src/InventarioLoading/index.jsx
@@ -1,9 +1,17 @@
+import ReactLoading from "react-loading";
+import InventarioLoading from "./InventarioLoading.css"
+
+
 function InventarioLoading() {
-    return (
+    return(
         <>
-        <div className="cargando">
-            <span>Estamos cargando tus items espera unos segundos :D </span>
-        </div>
+            <div className="d-flex justify-content-center gap-2">
+                <div className="spinner-border text-primary" role="status" >
+                </div>
+                <span className="text-primary">Estamos cargando tus items :D ...</span>
+            </div>
         </>
     )
 }
+
+export {InventarioLoading};
