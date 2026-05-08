bbb5b6bad98b977410add5dfca5e5252e69293f6
Elias Guardado <eliasjguardado2001@gmail.com>
2026-05-07 19:10:49 -0600

    Adding InventarioLoading Component the respective styles

---
+++ src/InventarioLoading/index.jsx
@@ -0,0 +1,9 @@
+function InventarioLoading() {
+    return (
+        <>
+        <div className="cargando">
+            <span>Estamos cargando tus items espera unos segundos :D </span>
+        </div>
+        </>
+    )
+}
